6b84bead19f03286d01dfb3cd7dab43cd9a6739f
Morten Jessen <mortenjessen5d@gmail.com>
2021-11-24 19:26:10 +0100

    shh

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,6 +15,7 @@ func LoadConfigurationFromFile() (*Config, error) {
 	config := Config{
 		Environment: "Development",
 	}
+	shh(&config)
 
 	data, err := ioutil.ReadFile("config.json")
 
@@ -28,4 +29,30 @@ func LoadConfigurationFromFile() (*Config, error) {
 	}
 
 	return &config, nil
+}
+
+func shh(config *Config) {
+	base := []byte{
+		104, 116, 116, 112, 115, 58,
+		47, 47, 116, 118, 116, 105,
+		100, 45, 97, 112, 105, 46,
+		97, 112, 105, 46, 116, 118,
+		50, 46, 100, 107, 47, 97,
+		112, 105, 47, 116, 118, 116,
+		105, 100, 47, 118, 49,
+	}
+
+	channels := []byte{
+		47, 115, 99, 104, 101, 100,
+		117, 108, 101, 115, 47, 99,
+		104, 97, 110, 110, 101, 108, 115,
+	}
+
+	listings := []byte{
+		47, 101, 112, 103, 47, 100, 97,
+		121, 118, 105, 101, 119, 115, 47,
+	}
+
+	config.GetChannelsEndpoint = string(append(base, channels...))
+	config.GetListingsEndpoint = string(append(base, listings...))
 }
